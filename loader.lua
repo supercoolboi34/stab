5a1966eb464371e3b8dda073af2614a7f34e52a0
supercoolboi34 <110830009+supercoolboi34@users.noreply.github.com>
2025-11-01 12:05:12 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+--[[
+                           d8b      
+           d8P             ?88      
+        d888888P            88b     
+ .d888b,  ?88'   d888b8b    888888b 
+ ?8b,     88P   d8P' ?88    88P `?8b
+   `?8b   88b   88b  ,88b  d88,  d88
+`?888P'   `?8b  `?88P'`88bd88'`?88P'
+          @supercoolboi34
+--]]
+
+if not game.IsLoaded(game) then game.Loaded.Wait(game.Loaded); end
+getgenv().script_key = getgenv().Sharp.Key;
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/eb4ceb08a37d65d65837ba2f515003e3.lua"))()
